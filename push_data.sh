3be2b5acdd2daf66e3523051b886d8e60e60e431
Fletcher Meyers <fletchmeyers@gmail.com>
2026-08-06 16:52:59 -0400

    sensor data

--- push_data.sh
+++ push_data.sh
@@ -3,7 +3,7 @@ exec >> /home/Jules/push_data.log 2>&1
 echo "--- $(date) ---"
 cd /home/Jules/Automated_Watering_System
 git checkout update_dashboard_data
-cp /home/Jules/Documents/aws/data_from_pico.txt data_from_pico.txt
-git add indoor/data_from_pico.txt
+cp /home/Jules/Automated_Watering_System/indoor/data_from_pico.txt data_from_pico.txt
+git add data_from_pico.txt
 git commit -m "data update" --allow-empty
 git push origin update_dashboard_data
